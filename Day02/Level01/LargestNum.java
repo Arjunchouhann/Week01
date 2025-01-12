39a9c72544e2a8d5813d772947993f2fd6891731
Arjunchouhann <chouhanarjun05112003@gmail.com>
2025-01-12 22:26:46 +0530

    Program Largest Number

---
+++ Day02/Level01/LargestNum.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+//Create Class LargestNum to find smallest number 
+public class LargestNum{
+	public static void main(String[]args){
+	//Create Scanner Object
+	Scanner sc = new Scanner (System.in);
+	System.out.println("Enter Three Numbers");
+	int number1 = sc.nextInt();
+	int number2 = sc.nextInt();
+	int number3 = sc.nextInt();
+	//Checking for finding largest number and print it using conditional operators
+	if((number1 > number2)&&(number1 > number3)){
+		System.out.println("Is the first number the Largest? Yes");
+	}
+	else if((number2 > number1)&&(number2 > number3)){
+		System.out.println("Is the Second number the Largest? Yes");
+		
+	}
+	else{
+		System.out.println("Is the third number the Largest? Yes");
+	}
+	}
+}
+		
