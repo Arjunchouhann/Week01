dfb950b484d749c86c44df177d1cdc0e6266d419
Arjunchouhann <chouhanarjun05112003@gmail.com>
2025-01-12 22:42:19 +0530

    Program Factor find

---
+++ Day02/Level02/FactorFind.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class FactorFind {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+		//create number variable to Get input for the number
+        System.out.print("Enter a number to find its factors: ");
+        int number = scanner.nextInt();
+		// Check if the number is positive
+        if (number <= 0) {
+            System.out.println("Please enter a positive integer greater than 0.");
+        } 
+		else {
+            System.out.println("Factors of " + number + " are:");
+
+            // Run a for loop to find factors
+            for (int i = 1; i < number; i++) {
+                // Check if the number is perfectly divisible by i
+                if (number % i == 0) {
+                    System.out.println(i); // Print the factor
+                }
+            }
+            // Optionally, print the number itself as a factor
+            System.out.println(number); // The number is also a factor of itself
+        }
+	}
+}
