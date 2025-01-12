c4ae53d47f5b2bde710110f1cbcc6f6e1bcf8365
Arjunchouhann <chouhanarjun05112003@gmail.com>
2025-01-13 00:43:00 +0530

    Program Vote Check Array

---
+++ Day03/Level01/VoteCheckArray.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class VoteCheckArray {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int Student [] = new int[10];
+        System.out.println(" Enter the Age of the 10 Student ");
+        for ( int i=0;i<Student.length;i++)
+        {
+            Student[i] = input.nextInt();
+        }
+        for (int i= 0 ; i< Student.length; i++ )
+        {
+            if(Student[i]<0)
+            {
+                System.out.println(" An invalid Age");
+            }
+            else if (Student[i]>=18)
+            {
+                System.out.println(" The Student with the age "+ Student[i] + " can vote");
+            }
+            else
+            {
+                System.out.println(" The student  with the age "+ Student[i] + " can not vote");
+            }
+        }
+    }
+}
