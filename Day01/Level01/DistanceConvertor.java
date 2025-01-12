4db349ec7db4ba93e348d4174c4191d50cc3526b
Arjunchouhann <chouhanarjun05112003@gmail.com>
2025-01-12 20:50:20 +0530

    Program Distance Calculator

---
+++ Day01/Level01/DistanceConvertor.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+//create DistanceConvertor Class to convert kilometers into miles
+public class DistanceConvertor {
+	public static void main ( String[]args ) {
+		//Create a variable kilometer to indicate distance in kilometers
+		double kilometer = 10.8 ;
+		//Create a variable miles to indicate distance in miles
+		double miles =  kilometer/1.6 ;
+		//Print the Distance
+		System.out.println("The Distance " + kilometer + " km in miles is " + miles);
+	}
+}
+		
