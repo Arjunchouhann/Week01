8f7c66f7dd3fdb7f9bf1e8d8174b80f5646b2c5c
Arjunchouhann <chouhanarjun05112003@gmail.com>
2025-01-12 22:49:03 +0530

    Program Multiplies Finder

---
+++ Day02/Level02/MultiplesFinder.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class MultiplesFinder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+		// Get input for the number
+        System.out.print("Enter a number to find its multiples below 100: ");
+        int number = scanner.nextInt();
+        // Check if the number is positive
+        if (number <= 0) {
+            System.out.println("Please enter a positive integer greater than 0.");
+        } else {
+            System.out.println("Multiples of " + number + " below 100 are:");
+            // Run a for loop backward from 100 to 1
+            for (int i = 100; i >= 1; i--) {
+                // Check if i is a multiple of the number
+                if (i % number == 0) {
+                    System.out.println(i); // Print the multiple
+                }
+            }
+        }
+	}
+}
